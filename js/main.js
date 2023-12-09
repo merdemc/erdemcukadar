4357d91339db7b1389b284dabda41438684833d8
merdemc <91091049+merdemc@users.noreply.github.com>
2023-12-09 19:38:46 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+// Setting time
+window.onload = function init() {
+    setInterval(myTimer, 1000);
+    function myTimer() {
+      const d = new Date();
+      document.getElementById("time").innerHTML = d.toLocaleTimeString();
+    }
+  };
+  
+
+
+
+/* const myHeading=document.querySelector(".visitor")
+myHeading.textContent="Visitor";
+
+
+const currentDate = new Date();
+
+
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+const dayOfWeek = daysOfWeek[currentDate.getDay()];
+const month = monthsOfYear[currentDate.getMonth()];
+const day = currentDate.getDate();
+const year = currentDate.getFullYear();
+
+const myTime=document.querySelector(".time")
+myTime.textContent=`Today is ${dayOfWeek},  ${day}/${month}/${year}`
+ */
