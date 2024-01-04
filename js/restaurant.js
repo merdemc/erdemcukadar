f5267951f91a469ebcf938b3fd63ef1d0bdb0dd4
merdemc <merdemc@gmail.com>
2024-01-04 22:34:59 +0300

    restaurant project added

---
+++ js/restaurant.js
@@ -0,0 +1,20 @@
+const costObj = {
+    food: {
+        kebap: 250,
+        pirzola: 275,
+        bonfile: 245,
+        makarna: 75,
+
+    },
+    foodProducts: {
+        nohut: 45,
+        fasulye: 55,
+        börülce: 35,
+        bulgur: 35,
+        prinç: 75,
+    }
+}
+
+console.log(costObj.foodProducts.börülce);
+
+console.log("ERDEM");
